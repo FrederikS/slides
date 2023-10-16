b6131cbbb14e6ab714ab406a02ee78db2c25a2a1
frederik.steffen <frederik.steffen@bayes.gg>
2023-10-16 17:37:33 +0200

    added java 21 slides

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,19 +1,46 @@
-import { poppins } from "@mdx-deck/themes";
+import { poppins as theme } from "@mdx-deck/themes";
 
 export default {
-  ...poppins,
+  ...theme,
   colors: {
     background: "#fff",
     text: "#000",
   },
   fonts: {
-    ...poppins.fonts,
+    ...theme.fonts,
     monospace: "'JetBrains Mono', monospace;",
   },
   styles: {
-    ...poppins.styles,
+    ...theme.styles,
     li: {
       margin: ".5em",
+    },
+    blockquote: {
+      fontStyle: "italic",
+      color: "#555555",
+      padding: "0.5em 30px 0 75px",
+      borderLeft: "8px solid #78C0A8",
+      lineHeight: "1.6",
+      position: "relative",
+      background: "#EDEDED",
+      "&:before": {
+        fontFamily: "Arial",
+        content: '"\\201C"',
+        color: "#78C0A8",
+        fontSize: "4em",
+        position: "absolute",
+        left: "10px",
+        top: "-10px",
+      },
+      // "&:after": {
+      //   fontFamily: "Arial",
+      //   content: '"\\201C"',
+      //   color: "#78C0A8",
+      //   fontSize: "4em",
+      //   position: "absolute",
+      //   right: "1rem",
+      //   bottom: "-1rem",
+      // },
     },
     Slide: {
       fontFamily: "body",
